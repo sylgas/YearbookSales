51862e6cbfc00b4561251e72e4e566286c27004c
Sylwia G¹sior <sgasior@avaya.com>
2017-05-24 12:15:10 +0200

    Remove unnecessary div

--- src/components/Common/Spinner.js
+++ src/components/Common/Spinner.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./common.less";
 import PropTypes from "prop-types";
 
-const Spinner = ({isLoading}) => (
-    <div>
-        {isLoading && (
-            <div className="spinner">
-                <i className="fa fa-spinner fa-spin fa-4x fa-fw"/>
-            </div>
-        )}
-    </div>
-);
+const Spinner = ({isLoading}) => {
+    if (!isLoading) {
+        return <div/>;
+    }
+
+    return (
+        <div className="spinner">
+            <i className="fa fa-spinner fa-spin fa-4x fa-fw"/>
+        </div>
+    );
+};
 
 Spinner.propTypes = {
     isLoading: PropTypes.bool

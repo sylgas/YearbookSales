f19f480816111cc5fcc9c0b126452a87db224117
Sylwia G¹sior <sgasior@avaya.com>
2017-05-25 16:45:37 +0200

    Add redux router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,18 @@ import "./app.less";
 import configureStore from "../../store/configureStore";
 import Provider from "react-redux/src/components/Provider";
 import Dashboard from "../Dashboard/Dashboard";
+import Router from "react-router/es/Router";
+import Route from "react-router/es/Route";
+import {hashHistory} from "react-router";
 
 const store = configureStore();
 
 const App = () => (
     <Provider store={store}>
-        <Dashboard/>
+        <Router history={hashHistory}>
+            <Route path="/" component={Dashboard}>
+            </Route>
+        </Router>
     </Provider>
 );
 
